0f809ae013c372c5c876da4f3386debc37dbb95e
Athyuttam Reddy <athyuttamre@gmail.com>
2016-03-31 11:11:11 -0400

    Switch to pm2, add .env template

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-setTimeout(function() {
-  console.log("Welcome to DN Bot!");
+require('dotenv').config();
+console.log("Welcome to DN Bot!");
+
+// Initialize database
+
+setInterval(function() {
+  // Make DN API request
+
+  // Get top ten stories
+
+  // Check if already published
+
+  // Post to Facebook
 }, 1500);
