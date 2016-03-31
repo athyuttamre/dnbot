7f270ca318f4e2624c16b48e6bef6403f9f12816
Athyuttam Reddy <athyuttamre@gmail.com>
2016-03-31 19:17:36 -0400

    Set up database

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 require('dotenv').config();
-console.log("Welcome to DN Bot!");
+
+// Set up database
+var fs = require("fs");
+var dbFile = "./dnbot-db.db";
+var exists = fs.existsSync(dbFile);
+
+if(!exists) {
+  console.log("Creating DB file.");
+  fs.openSync(dbFile, "w");
+}
+
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(dbFile);
 
 // Initialize database
+db.serialize(function() {
+  if(!exists) {
+    db.run("CREATE TABLE Stories (id TEXT, title TEXT, comment TEXT, url TEXT)");
+  }
+
+  insertStory("0", "Test Story", "");
+});
+
+console.log("Welcome to DN Bot!");
 
 setInterval(function() {
   // Make DN API request
+  console.log("Making DN API Request");
 
   // Get top ten stories
+  console.log("Got top ten stories!");
 
   // Check if already published
+  console.log("Checking if already published");
 
   // Post to Facebook
+  console.log("Posting to Facebook");
+
+  console.log("\n");
 }, 1500);
